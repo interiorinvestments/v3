886df745446c78088f82f7ac837f697fb21546a1
Alex Matseshe <amatseshe@gmail.com>
2020-10-02 10:27:23 -0500

    Add covid page

---
+++ src/views/inform/CovidView/index.js
@@ -0,0 +1,16 @@
+import { Container, Grid } from '@material-ui/core';
+import SectionHeader from 'components/molecules/SectionHeader';
+
+const CovidView = () => (
+  <Container>
+    <SectionHeader
+      title="How We’re Helping Around the World"
+      subtitle="For more than 100 years, Herman Miller has used the power of design to serve humankind. During this unprecedented global crisis, we’re using creativity, collaboration, and hard work to help stop the spread of the coronavirus—and keep our communities healthy.
+
+Here’s the latest, most important information about our efforts and our company."
+      label="the Coronavirus Crisis"
+    />
+  </Container>
+);
+
+export default CovidView;
